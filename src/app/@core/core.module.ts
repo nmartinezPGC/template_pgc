a0f835f94515bb54ab4995e3ba710070747de5c8
nmartinezPGC <nahum.sreci@gmail.com>
2018-07-01 16:43:10 -0600

    KK
    KK

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,8 +1,8 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbAuthModule, 
-        NbDummyAuthStrategy, 
-        NbPasswordAuthStrategy, 
+import { NbAuthModule,
+        NbDummyAuthStrategy,
+        NbPasswordAuthStrategy,
         NbAuthJWTToken } from '@nebular/auth';
 
 import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
