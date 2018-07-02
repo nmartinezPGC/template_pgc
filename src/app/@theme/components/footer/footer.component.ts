c3d66d599425b6c41edf704ad234948ba90c62b7
Nahum Martinez <nahum.sreci@gmail.com>
2018-07-02 16:51:44 -0600

    SSS
    SSS

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by">&copy; 2018 <b><a href="http://sreci.gob.hn" target="_blank">Secretaría de Relaciones Exteriores y Cooperación Internacional | SRECI </a></b> v1.0.0 </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
