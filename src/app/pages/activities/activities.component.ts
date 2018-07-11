f39570db5cfc5aa4e1a2e55a17f32c0f37af823a
Nahum Martinez <nahum.sreci@gmail.com>
2018-07-11 16:35:27 -0600

    NAM | 2018-17-11
    Template

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-activities',
-  template: ` <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item"><a href="#/">Actividades</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Ingreso de Actividad</li>
-                </ol>
-              </nav>
+  template: `
     <router-outlet></router-outlet>
   `,
 })
