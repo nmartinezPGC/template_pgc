b42cea20de492334cf76f9f13f86916953c5ee4d
nmartinezPGC <nahum.sreci@gmail.com>
2018-07-31 22:43:35 -0600

    NAMv2

--- src/app/pages/activities/new-activity/new-activity.component.ts
+++ src/app/pages/activities/new-activity/new-activity.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../../../@core/data/users.service';
 
