37f967e5b708786093eea1667598a8d0a846ead0
Nahum Martinez <nahum.sreci@gmail.com>
2018-08-20 16:55:55 -0600

    NAM 2018-08-20 v1

--- src/app/pages/activities/activities.module.ts
+++ src/app/pages/activities/activities.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 
+// Imports de Tabla Smart de Angular
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { SmartTableService } from '../../@core/data/smart-table.service';
+
+// Imports de Toaster-Notification Service
+import { ToasterModule } from 'angular2-toaster';
+
 import { ThemeModule } from '../../@theme/theme.module';
 import { ActivitiesRoutingModule, routedComponents } from './activities-routing.module';
 
@@ -7,9 +14,14 @@ import { ActivitiesRoutingModule, routedComponents } from './activities-routing.
   imports: [
     ThemeModule,
     ActivitiesRoutingModule,
+    Ng2SmartTableModule, // Modulo Base del pluguin de TableSmart
+    ToasterModule.forRoot(), // Modulo de Toaster-Notification
   ],
   declarations: [
     ...routedComponents,
+  ],
+  providers: [
+    SmartTableService, // Defincion del Servicio que provee los Datos de la Tabla: ID's Internas
   ],
 })
 export class ActivitiesPGCModule {}
--- src/app/pages/activities/new-activity/new-activity.component.ts
+++ src/app/pages/activities/new-activity/new-activity.component.ts
@@ -3,13 +3,18 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 // Servicios que la Clase nesesitara para su funcionanmiento
 import { UserService } from '../../../@core/data/users.service'; // Servicio de Usuarios
 import { ToasterService, ToasterConfig, Toast, BodyOutputType } from 'angular2-toaster'; // Servicio de Notificaciones
+// import 'style-loader!angular2-toaster/toaster.css';
+import { LocalDataSource } from 'ng2-smart-table'; // DataLocal de Ejemplo para el JSON de envio
+import { SmartTableService } from '../../../@core/data/smart-table.service'; // Servicio de la SmartTable de la API
+
+import 'style-loader!angular2-toaster/toaster.css';
 
 @Component({
   selector: 'ngx-new-activity',
   templateUrl: './new-activity.component.html',
-  styleUrls: ['./new-activity.component.scss'],
+  styleUrls: ['./new-activity.component.scss', '../../components/notifications/notifications.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [ ToasterService ],
+  // providers: [ ToasterService, SmartTableService ],
 })
 export class NewActivityComponent implements OnInit {
   /****************************************************************************
@@ -19,13 +24,14 @@ export class NewActivityComponent implements OnInit {
   * privada
   * Objetivo: Tener el acceso a todas las variables de la Clase
   ****************************************************************************/
+ // Configuracion del Toaster-Notifications
   config: ToasterConfig;
 
   position = 'toast-top-right';
   animationType = 'fade';
-  title = 'HI there!';
+  title = 'HI there! soy Nahum Martinez, creador de la PGC';
   content = `I'm cool toaster!`;
-  timeout = 5000;
+  timeout = 10000;
   toastsLimit = 5;
   type = 'default';
 
@@ -34,6 +40,57 @@ export class NewActivityComponent implements OnInit {
   isDuplicatesPrevented = false;
   isCloseButton = true;
 
+  // Configuracion de la SmartTable
+  data: any;
+
+  settings = {
+    add: {
+      confirmCreate: true,
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    actions: {
+      columnTitle: 'Acciones',
+      add: true,
+      delete: true,
+      edit: true
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+        width: '20%',
+      },
+      firstName: {
+        title: 'Organización',
+        width: '80%',
+        edit: false,
+        /*editor: {
+          type: 'completer',
+          config: {
+            completer: {
+              data: this.data,
+              searchFields: 'firstName',
+              titleField: 'firstName',
+              descriptionField: 'firstName',
+            },
+          },
+        },*/
+      },
+    },
+  };
+  
+  source: LocalDataSource = new LocalDataSource();
 
   // private toasterService: ToasterService;
 
@@ -48,7 +105,11 @@ export class NewActivityComponent implements OnInit {
   * Objetivo: constructor in the method header API
   ****************************************************************************/
   constructor( private _userService: UserService,
-               private _toasterService: ToasterService ) {
+               private _toasterService: ToasterService,
+               private service: SmartTableService ) {
+    this.data = this.service.getData();
+    // console.log( this.data );
+    this.source.load( this.data );
       // Inicializa el ToasterService
     // this.toasterService = _toasterService;
 
@@ -78,6 +139,7 @@ export class NewActivityComponent implements OnInit {
   ****************************************************************************/
   makeToast() {
     this.showToast(this.type, this.title, this.content);
+    // console.log('Opcion de Toaster 1.3 ' + JSON.stringify(this.content));
   } // FIN | makeToast
 
 
@@ -88,7 +150,7 @@ export class NewActivityComponent implements OnInit {
   * Descripcion: Method showToast of the Class
   * Objetivo: showToast in the method header API
   ****************************************************************************/
-  private showToast(type: string, title: string, body: string) {
+  private showToast(type: string, title: string, body: string) {    
     this.config = new ToasterConfig({
       positionClass: this.position,
       timeout: this.timeout,
@@ -97,7 +159,7 @@ export class NewActivityComponent implements OnInit {
       preventDuplicates: this.isDuplicatesPrevented,
       animation: this.animationType,
       limit: this.toastsLimit,
-    });
+    });    
     const toast: Toast = {
       type: type,
       title: title,
@@ -106,14 +168,66 @@ export class NewActivityComponent implements OnInit {
       showCloseButton: this.isCloseButton,
       bodyOutputType: BodyOutputType.TrustedHtml,
     };
-    this._toasterService.popAsync(toast);
+    // this._toasterService.popAsync(toast);
+    this._toasterService.pop(toast);
   } // FIN | showToast
 
+
+  /****************************************************************************
+  * Funcion: toasterconfig
+  * Object Number: 004.1
+  * Fecha: 16-08-2018
+  * Descripcion: Method showToast of the Class
+  * Objetivo: showToast in the method header API
+  ****************************************************************************/
+  public toasterconfig: ToasterConfig =
+    new ToasterConfig({
+      showCloseButton: { 'warning': true, 'error': false },
+    }); // FIN | toasterconfig
+
+
+  /****************************************************************************
+  * Funcion: onDeleteConfirm
+  * Object Number: 005
+  * Fecha: 20-08-2018
+  * Descripcion: Method para Borrar Items de la SmartTable
+  * Objetivo: enviar al Json de ID'Intermas la información que ocupa la API
+  ****************************************************************************/
+  onDeleteConfirm(event): void {
+    if (window.confirm('¿ Estas seguro de borrar la Organización ?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  } // FIN | onDeleteConfirm
+
+
+  /****************************************************************************
+  * Funcion: onCreateConfirm
+  * Object Number: 006
+  * Fecha: 20-08-2018
+  * Descripcion: Method para Borrar Items de la SmartTable
+  * Objetivo: enviar al Json de ID'Intermas la información que ocupa la API
+  ****************************************************************************/
+  onCreateConfirm(event) {
+    if (window.confirm('¿ Estas seguro de agregar una organización ?')) {
+      // event.newData['firstName'] += ' + added in code';
+      event.confirm.resolve(event.newData);
+      // console.log('Dato de la Fila Nueva ' + event.newData.id );
+      const parseEvent: any = JSON.stringify(event.newData);
+      // console.log('onCreateConfirm +++ ' + parseEvent);
+    } else {
+      event.confirm.reject();
+    }
+  } // FIN | onDeleteConfirm
+
+  
+  /* ********************************************************************************************************** */
   /* ***************************** Funciones Propias de la Clase ************************************************/
 
   /****************************************************************************
   * Funcion: userDatailsService
-  * Object Number: 005
+  * Object Number: 00
   * Fecha: 16-08-2018
   * Descripcion: Method userDatailsService of the Class
   * Objetivo: userDatailsService detalle del Usuario llamando a la API
@@ -136,11 +250,5 @@ export class NewActivityComponent implements OnInit {
       },
     );
   } // FIN | userDatailsService
-
-
-  public toasterconfig1: ToasterConfig =
-    new ToasterConfig({
-      showCloseButton: { 'warning': true, 'error': false },
-    });
 
 }
