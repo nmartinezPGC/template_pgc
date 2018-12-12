f3fb7bb912d4af48beb10ca44689b9624de486f3
Nahum Martinez <nahum.sreci@gmail.com>
2018-12-12 14:22:52 -0600

    NAM | 2018-12-12 | Se sube la Estrcutura de Mantenimientos v3

--- src/app/pages/mantenimientos/mantenimientos.component.ts
+++ src/app/pages/mantenimientos/mantenimientos.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mantenimientos',
-  templateUrl: './mantenimientos.component.html',
-  styleUrls: ['./mantenimientos.component.scss'],
 })
 export class MantenimientosComponent implements OnInit {
 
