89ec5b14b0f17ae2ec33d48e52ff17d961c66abf
Edgar Ramirez <edgar.ramirez.pgc@gmail.com>
2018-12-18 15:15:27 -0600

    EDR| 2018-12-18| creacion componente de perfil

---
+++ src/app/pages/mantenimientos/mant-seguridad/perfiles/perfiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'perfiles',
+  templateUrl: './perfiles.component.html',
+  styleUrls: ['./perfiles.component.scss']
+})
+export class PerfilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
