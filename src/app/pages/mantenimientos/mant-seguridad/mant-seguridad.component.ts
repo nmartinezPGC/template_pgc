15bf5a63a50a0b22b7aead7220956985774fb855
RicardoPavonPgc <ricardo.pavon2018.pgc@gmail.com>
2018-12-18 14:21:09 -0600

    DAP| 2018-12-18| creacion de componentes

---
+++ src/app/pages/mantenimientos/mant-seguridad/mant-seguridad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mant-seguridad',
+  templateUrl: './mant-seguridad.component.html',
+  styles: []
+})
+export class MantSeguridadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
