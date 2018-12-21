788c9ad2899871e1a94787f98b786350c91566ee
RicardoPavonPgc <ricardo.pavon2018.pgc@gmail.com>
2018-12-21 14:40:16 -0600

    DAP| 2018-12-21| maquetacion de mantenimientos de perfiles

--- src/app/pages/mantenimientos/mant-seguridad/perfiles/perfiles.component.ts
+++ src/app/pages/mantenimientos/mant-seguridad/perfiles/perfiles.component.ts
@@ -22,7 +22,7 @@ export class PerfilesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.fillDataTable();
+    // this.fillDataTable();
   }
 
   /*
@@ -30,24 +30,24 @@ export class PerfilesComponent implements OnInit {
    * Descripcion: Realiza el llenado de la Tabla: Perfiles con Todos los Filtros
    * Params: null
   **/
-  fillDataTable() {
-    setTimeout(function () {
-      $(function () {
-        $('#table').DataTable( {
-            dom: 'Bfrtip',
-            fixedHeader: true,
-            "autoWidth": false,
-            // Tamaño de la Pagina
-            "pageLength": 10,
-            // Refresca la Data y Borra de Memoria los Datos anteriores
-            destroy: true,
-            retrieve: true,
-    
-            //Selecciona las Filas
-            select: true
-        } );
-        
-      });
-    }, 500);
-  }
+  // fillDataTable() {
+  //   setTimeout(function () {
+  //     $(function () {
+  //       $('#table').DataTable({
+  //         dom: 'Bfrtip',
+  //         fixedHeader: true,
+  //         'autoWidth': false,
+  //         // Tamaño de la Pagina
+  //         'pageLength': 10,
+  //         // Refresca la Data y Borra de Memoria los Datos anteriores
+  //         destroy: true,
+  //         retrieve: true,
+
+  //         // Selecciona las Filas
+  //         select: true,
+  //       });
+
+  //     });
+  //   }, 500);
+  // }
 }
