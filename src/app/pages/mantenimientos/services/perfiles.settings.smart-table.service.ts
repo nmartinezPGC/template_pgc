067b717a82994774126901de5cd4f64def931e86
Edgar Ramirez <edgar.ramirez.pgc@gmail.com>
2019-01-10 15:56:57 -0600

    EDR| 2019-01-10|  creacion de archivo perfiles.settings.smart-table

--- src/app/pages/mantenimientos/services/perfiles.settings.smart-table.service.ts
+++ src/app/pages/mantenimientos/services/perfiles.settings.smart-table.service.ts
@@ -7,87 +7,81 @@
  * @fecha 10/01/2019
  */
 
- import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
- @Injectable({
-     providedIn: 'root'
- })
- export class configSmartTableService {
-     //Definicion de las variables Globales 
-    public settings: any;
-    
+@Injectable({
+  providedIn: 'root',
+})
+export class configSmartTableService {
+  // Definicion de las variables Globales
+  public settings: any;
+  constructor() { }
 
-    constructor() {}
+  /****************************************************************************
+* Funcion: configSmartTable
+* Object Number: 001
+* Fecha: 10-01-2019
+* Descripcion: Method ConfigSmartTableService of the Class
+* Objetivo: Establecer las configuraciones para la Smart Table
+****************************************************************************/
 
-    /****************************************************************************
-  * Funcion: configSmartTable
-  * Object Number: 001
-  * Fecha: 10-01-2019
-  * Descripcion: Method ConfigSmartTableService of the Class
-  * Objetivo: Establecer las configuraciones para la Smart Table
-  ****************************************************************************/
-  
-    configSmartTable(smartTable: string, smartNormal: number){
-    
-    
-
+  configSmartTable(smartTable: string, smartNormal: number) {
     switch (smartTable) {
       case 'userSmart':
         switch (smartNormal) {
           case 1:
-          
-          this.settings = {
-            hideSubHeader: false,
-        
-            add: {
-              addButtonContent: '<i class="nb-plus"></i>',
-              createButtonContent: '<i class="nb-checkmark"></i>',
-              cancelButtonContent: '<i class="nb-close"></i>',
-            },
-            edit: {
-              editButtonContent: '<i class="nb-edit"></i>',
-              saveButtonContent: '<i class="nb-checkmark"></i>',
-              cancelButtonContent: '<i class="nb-close"></i>',
-              confirmSave: true,
-            },
-            delete: {
-              deleteButtonContent: '<i class="nb-trash"></i>',
-              confirmDelete: true,
-            },
-            columns: {
-              // idPerfil: {
-              // title: 'Id de Perfil',
-              // notShownField: true,
-        
-              // valuePrepareFunction: (edit) => {
-              // console.log(edit);
-              // this.onSaveConfirm(edit);
-              // },
-              // },
-              codPerfil: {
-                title: 'Codigo de Perfil',
-                type: 'string',
+            this.settings = {
+              hideSubHeader: false,
+
+              add: {
+                addButtonContent: '<i class="nb-plus"></i>',
+                createButtonContent: '<i class="nb-checkmark"></i>',
+                cancelButtonContent: '<i class="nb-close"></i>',
               },
-              descPerfil: {
-                title: 'Descripcion de Perfil',
-                type: 'string',
+              edit: {
+                editButtonContent: '<i class="nb-edit"></i>',
+                saveButtonContent: '<i class="nb-checkmark"></i>',
+                cancelButtonContent: '<i class="nb-close"></i>',
+                confirmSave: true,
               },
-              // Json anidado para lograr capturar el valor de una entidad
-              // bibliografia : https://github.com/akveo/ng2-smart-table/issues/375
-              descripcionTipoPerfil: {
-                valuePrepareFunction: (cell: any, row: any) =>   row.idTipoPerfil.descTipo ,
-                title: 'Tipo',
-                type: 'string',
+              delete: {
+                deleteButtonContent: '<i class="nb-trash"></i>',
+                confirmDelete: true,
               },
-            },
-          };
+              columns: {
+                // idPerfil: {
+                // title: 'Id de Perfil',
+                // notShownField: true,
+
+                // valuePrepareFunction: (edit) => {
+                // console.log(edit);
+                // this.onSaveConfirm(edit);
+                // },
+                // },
+                codPerfil: {
+                  title: 'Codigo de Perfil',
+                  type: 'string',
+                },
+                descPerfil: {
+                  title: 'Descripcion de Perfil',
+                  type: 'string',
+                },
+                // Json anidado para lograr capturar el valor de una entidad
+                // bibliografia : https://github.com/akveo/ng2-smart-table/issues/375
+                descripcionTipoPerfil: {
+                  valuePrepareFunction: (cell: any, row: any) => row.idTipoPerfil.descTipo,
+                  title: 'Tipo',
+                  type: 'string',
+                },
+              },
+            };
             break;
 
-           case 2:
-           break; 
+          case 2:
+            break;
         }
         break;
-      }
-
     }
- }
+
+  }
+}
