58e0804bb25a54a8b1c0464196be81e02294e95d
Edgar Ramirez <edgar.ramirez.pgc@gmail.com>
2019-01-14 15:11:59 -0600

    EDR| 2019-01-14|  modificacion de estructura de usuarios

--- src/app/pages/mantenimientos/mant-seguridad/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/mant-seguridad/usuarios/usuarios.component.ts
@@ -1,5 +1,5 @@
 /**
- * @author David Pavon
+ * @author Edgar Ramirez
  * @returns mantenimientos de usuario
  * @name UsuariosComponent
  * @alias _usuariosComponent
@@ -17,6 +17,60 @@ declare var $: any;
   styleUrls: ['./usuarios.component.scss'],
 })
 export class UsuariosComponent implements OnInit {
+  // estructura de la smart table donde mostramos los datos de los usuarios
+  settings = {
+    hideSubHeader: false,
+
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmSave: true,
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      nombre1: {
+        title: 'Primer Nombre',
+        type: 'string',
+      },
+      nombre2: {
+        title: 'Segundo Nombre',
+        type: 'string',
+      },
+      apellido1: {
+        title: 'Primer Apellido',
+        type: 'string',
+      },
+      apellido2: {
+        title: 'Segundo Apellido',
+        type: 'string',
+      },
+      email: {
+        title: 'Email',
+        type: 'string',
+      },
+      password: {
+        title: 'Password',
+        type: 'string',
+      },
+      tipUsuario: {
+        title: 'Tipo de Usuario',
+        type: 'string',
+      },
+      pais: {
+        title: 'Pais',
+        type: 'string',
+      },
+    },
+  };
 
   constructor() { }
 
