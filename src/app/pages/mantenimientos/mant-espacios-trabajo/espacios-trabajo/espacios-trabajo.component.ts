2057d7dd563b5a0e79cf2eb22e3c543854e87a66
Edgar Ramirez <edgar.ramirez.pgc@gmail.com>
2019-02-13 19:00:56 -0600

    EDR| 2019-02-13|  modificacion en los archivos de usuarios

--- src/app/pages/mantenimientos/mant-espacios-trabajo/espacios-trabajo/espacios-trabajo.component.ts
+++ src/app/pages/mantenimientos/mant-espacios-trabajo/espacios-trabajo/espacios-trabajo.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'espacios-trabajo',
+  selector: 'ngx-espacios-trabajo',
   templateUrl: './espacios-trabajo.component.html',
-  styleUrls: ['./espacios-trabajo.component.scss']
+  styleUrls: ['./espacios-trabajo.component.scss'],
 })
 export class EspaciosTrabajoComponent implements OnInit {
 
