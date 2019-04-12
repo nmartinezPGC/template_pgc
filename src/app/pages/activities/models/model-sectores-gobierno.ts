30bd1164428ce019fa4b8c6e6cd7e1ab211845b7
EdgarRamirezPgc <edgar.ramirez.pgc@gmail.com>
2019-04-12 17:45:48 -0500

    EDR|2019-04-12| creacion de modelos

---
+++ src/app/pages/activities/models/model-sectores-gobierno.ts
@@ -0,0 +1,30 @@
+/**
+ * @author Edgar Ramirez
+ * @returns Modelo de los ODS
+ * @name ActivitySectoresGobiernoModel
+ * @alias _activitySectoresGobiernoModel
+ * @version 1.0.0
+ * @fecha 12/04/2019
+ */
+export class ActivitySectoresGobiernoModel {
+    // Creacion del Constructor de la Clase
+    constructor(
+        // Definicion de las variables Mapeadas a la Clase del Model
+        // Nivel de Implementacion
+        public idSector:  number,
+        public codigoSector: string,
+
+
+        public idTipoSector: {idTipo: number},
+        public idTipo: number,
+        public sectorPadreId: {idSector: number},
+        public idSectorPadre: number,
+        public nombreSector: string,
+        public descripcionSector: string,
+        public activo: boolean,
+        public fechaCreacion: Date,
+        public idNivelSector: { idNivel: number },
+        public idNivel: number,
+
+    ) { }
+}
