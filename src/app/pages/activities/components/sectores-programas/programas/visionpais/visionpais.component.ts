158df5dd135420e1757de0fe14e433d5f333ae64
Allan <allan.madrid.pgc@gmail.com>
2019-04-12 16:11:41 -0600

    AMA |2019-04-12| Creación Componente Vision Pais

--- src/app/pages/activities/components/sectores-programas/programas/visionpais/visionpais.component.ts
+++ src/app/pages/activities/components/sectores-programas/programas/visionpais/visionpais.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-visionpais',
   templateUrl: './visionpais.component.html',
-  styleUrls: ['./visionpais.component.scss']
+  styleUrls: ['./visionpais.component.scss'],
 })
 export class VisionpaisComponent implements OnInit {
 
